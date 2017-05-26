2ac6b9d89ec5fff251d4fbe07c9f6218e21a2b20
taoyuyu <taoyu@whu.edu.cn>
2017-05-26 18:29:16 +0800

    说明

--- java/longestIncresingSqu.java
+++ java/longestIncresingSqu.java
@@ -2,6 +2,11 @@ package taoyu.com.LIS;
 
 /**
  * 最长递增子序列
+ * 思路：
+ * 长度为k的序列s[1...k]，构造一个lis[1...k]，lis[i]表示以s[i]结尾的最长子序列的长度。
+ * 对于s[i+1]，只需要检查在它前面小于它的元素s[j]，lis[i+1] = max{ lis[j] } + 1, ( 0<=j<=i )；时间复杂度为O(n^2)；
+ *
+ * 算法优化：求解list如何提速?
  */
 public class longestIncresingSqu {
     public static void main(String[] args) {
